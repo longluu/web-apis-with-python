ddc37cb74b2f517b1115755fd87c1d2b91f8f859
longluu <thelong20.4@gmail.com>
2022-02-28 15:18:31 -0500

    intermediate commit

--- app.py
+++ app.py
@@ -12,9 +12,8 @@ def index():
     This method will
     1. Provide usage instructions formatted as JSON
     """
-    # response = {"usage": "/dict?=<word>"}
-    # Since this is a website with front-end, we don't need to send the usage instructions
-    return "TODO"
+    response = {"usage": "/dict?=<word>"}
+    return jsonify(response)
 
 
 @app.get("/dict")
@@ -26,7 +25,30 @@ def dictionary():
     2. Try to find an exact match, and return it if found
     3. If not found, find all approximate matches and return
     """
-    return "TODO"
+
+    # Get a word
+    words = request.args.getlist("word")
+
+    # Error if no input
+    if not words:
+        return jsonify({"status": "error", "word": words, "data": "No word provided."})
+
+    # Iterate over all words
+    response = {"words": []}
+    for word in words:
+        # Check exact match in dictionary
+        definitions = match_exact(word)
+        if definitions:
+            response["words"].append({"status": "success", "word": word, "data": definitions})
+        else:
+            # Check approximate match
+            definitions = match_like(word)
+            if definitions:
+                response["words"].append({"status": "partial", "word": word, "data": definitions})
+            else:
+                return jsonify({"status": "error", "word": word, "data": "word not found"})
+
+    return jsonify(response)
 
 
 if __name__ == "__main__":
